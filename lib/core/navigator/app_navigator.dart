fe3f2aebab827c63ea8aab6b547dee381c5e94a8
Rohan Sunar <rohanksunar15@gmail.com>
2025-06-21 17:19:39 +0530

    add file_navigator and size_config files

---
+++ lib/core/navigator/app_navigator.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+class AppNavigator {
+  static void pushToScreen(BuildContext context, Widget screen) {
+    Navigator.of(context).push(
+      PageRouteBuilder(
+        pageBuilder: (context, animation, secondaryAnimation) => screen,
+        transitionDuration: const Duration(milliseconds: 300),
+        transitionsBuilder: (context, animation, secondaryAnimation, child) {
+          const begin = Offset(1.0, 0.0);
+          const end = Offset.zero;
+          const exitBegin = Offset.zero;
+          const exitEnd = Offset(-0.3, 0.0); 
+          const curve = Curves.easeInOut;
+
+          final tween = Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
+          final exitTween = Tween(begin: exitBegin, end: exitEnd).chain(CurveTween(curve: curve));
+
+          return SlideTransition(
+            position: animation.drive(tween),
+            child: SlideTransition(
+              position: secondaryAnimation.drive(exitTween),
+              child: child,
+            ),
+          );
+        },
+      ),
+    );
+  }
+
+  static void pushReplacementScreen(BuildContext context, Widget screen) {
+    Navigator.pushReplacement(
+      context,
+      PageRouteBuilder(
+        pageBuilder: (context, animation, secondaryAnimation) => screen,
+        transitionsBuilder: (_, animation, __, child) {
+          const begin = Offset(1.0, 0.0);
+          const end = Offset.zero;
+          const curve = Curves.easeInOut;
+
+          final tween = Tween(
+            begin: begin,
+            end: end,
+          ).chain(CurveTween(curve: curve));
+          final offsetAnimation = animation.drive(tween);
+
+          return SlideTransition(position: offsetAnimation, child: child);
+        },
+      ),
+    );
+  }
+
+  static void popScreen(BuildContext context) {
+    Navigator.pop(context);
+  }
+}
